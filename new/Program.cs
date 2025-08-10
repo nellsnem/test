e0aa253849704451bea613cfca00acae68b7c43e
Snizhana <herasymenko.snizhana1123@vu.cdu.edu.ua>
2025-08-10 23:54:46 +0300

    first commit

---
+++ new/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+interface IAnimal
+{
+    void Speak();
+}
+
+class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine(" meow");
+    }
+}
+class Cat : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine(" gaf");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IAnimal dog = new Dog();
+        IAnimal cat = new Cat();
+
+        dog.Speak();
+        cat.Speak();
+        
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
